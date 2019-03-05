4deac7c553a1b67c5412a9a1b9e75d8e0be3e4cf
Kolchin <skolchin@kpmg.ru>
2019-03-05 14:55:06 +0300

    Initial commit. Includes Plot 1 creation script.

---
+++ R/plot1.r
@@ -0,0 +1,35 @@
+#
+# Coursera Explanatory Data Analysis week 1 assignment
+# Plot 1. Global active power vs Frequence histogramm
+#
+library(readr)
+
+# Load the dataset
+rdata <- read_delim(
+  "data/household_power_consumption.zip", 
+  delim = ";",
+  col_types = cols(
+    col_date(format = "%d/%m/%Y"),
+    col_time(),
+    col_double(),
+    col_double(),
+    col_double(),
+    col_double(),
+    col_double(),
+    col_double(),
+    col_double()
+  ),
+  na = c("", "NA", "?")
+  )
+
+# Make a histogram and save it to a PNG file
+png(filename = "figure/plot1.png", width = 480, height = 480)
+with(
+  subset(rdata, Date >= "2007/02/01" & Date <= "2007/02/02"),
+  hist(Global_active_power, 
+       xlab = "Global active power (kilowatts)",
+       main = "Global Active Power",
+       col = "red"
+       )
+)
+dev.off()
